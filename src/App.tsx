a4eddce98f7f6d162cf6ee699fb06e687ad6344c
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-11 01:21:54 -0300

    yeah

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ function App() {
     async function getPosts() {
       const res = await fetch("http://localhost:3000/posts");
       const data = await res.json();
-      data[2].published = true;
       setPosts(data.filter((p: post) => p.published === true));
     }
     getPosts().catch((err) => console.log(err));
