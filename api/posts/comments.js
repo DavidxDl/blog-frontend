c8d306479b042b4d1b5eff39a1d51aa99d690972
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-14 17:23:59 -0300

    added another route

---
+++ api/posts/comments.js
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+export default async (req, res) => {
+  try {
+    const { postId } = req.query;
+    const response = await fetch(
+      `http://172.233.16.85/posts/${postId}/comments`,
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "internal server error" });
+  }
+};
