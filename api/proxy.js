00665a05d969930171fcdbcb9853634463cb93f7
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-14 16:57:24 -0300

    added more routes to the proxy

--- api/proxy.js
+++ api/proxy.js
@@ -2,10 +2,32 @@ import fetch from "node-fetch";
 
 export default async (req, res) => {
   try {
-    const response = await fetch("http://172.233.16.85/posts");
+    const { path } = req;
+    let url = "http://172.233.16.85";
+
+    if (path.startsWith("/posts")) {
+      // If the request is for /posts or /posts/postId
+      const parts = path.split("/");
+      if (parts.length >= 3 && parts[2]) {
+        // If there's a postId specified
+        url += `/posts/${parts[2]}`;
+        if (parts.length >= 4 && parts[3] === "comments") {
+          // If the request is for /posts/postId/comments
+          url += "/comments";
+        }
+      } else {
+        // If the request is for /posts (list of posts)
+        url += "/posts";
+      }
+    }
+
+    // Fetch data from the constructed URL
+    const response = await fetch(url);
     const data = await response.json();
+
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ error: "internal server error" });
+    console.error("Error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
