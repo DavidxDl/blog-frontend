a6a7e0c9a10d2f20081096a2856704f8873ddc4b
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-14 16:50:17 -0300

    fix some naming conflicts

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,9 @@
-import {fetch} from "node-fetch"
+import fetch from "node-fetch";
 
 export default async (req, res) => {
   try {
-    const res = await fetch("http://172.233.16.85/posts");
-    const data = await res.json();
+    const response = await fetch("http://172.233.16.85/posts");
+    const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: "internal server error" });
