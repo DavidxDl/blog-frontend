518b9fcf4457d77c3225efee1100dd1851279d57
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-14 22:31:27 -0300

    testing again

--- api/posts/[postId].comments.js
+++ api/posts/[postId].comments.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 export default async (req, res) => {
   try {
     //yep
-    const { postId } = req.params.postId;
+    const { postId } = req.params.postId.split(".")[0];
     const response = await fetch(
       `http://172.233.16.85/posts/${postId}/comments`,
     );
