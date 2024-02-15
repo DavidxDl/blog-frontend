cbdde2e50e039166962f5622b9091df9b79056c4
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-14 23:35:06 -0300

    testing the api

--- api/posts/[postId].js
+++ api/posts/[postId].js
@@ -3,10 +3,12 @@ import fetch from "node-fetch";
 export default async (req, res) => {
   try {
     const { postId } = req.query;
+    console.log(postId);
     const response = await fetch(`http://172.233.16.85/posts/${postId}`);
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ error: "internal server error" });
+    const { postId } = req.query;
+    res.status(500).json({ postId: postId, error: "internal server error" });
   }
 };
